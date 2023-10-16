02c9e3630fd973dc88b2b9fb169c897a17d386df
g21turbo <dg21stew@gmail.com>
2023-10-15 20:25:37 -0700

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,26 @@ public class Main {
         long long2 = 25;
         byte byte2 = 15;
 
-        // Expressions
+        // Expression = kilometers = (100 * 1.609344)
         double kilometers = (100 * 1.609344);
 
+        // Expression = highScore = 50
         int highScore = 50;
 
+        // Expression = highScore > 25
         if (highScore > 25) {
+
+            // Expression = 1000 + highScore, highScore = 1000 + highScore
             highScore = 1000 + highScore; //add bonus points
         }
 
+        // Expression = health = 100
         int health = 100;
-        
+
+        // Expression = health < 25, highScore > 1000, (health < 25) && (highScore > 1000)
         if ((health < 25) && (highScore > 1000)) {
+
+            // Expression = highScore - 1000, highScore = highScore - 1000
             highScore = highScore - 1000;
         }
 
